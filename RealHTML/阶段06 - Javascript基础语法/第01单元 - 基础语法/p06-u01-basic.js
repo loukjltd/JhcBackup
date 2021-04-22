1de0e99aa1c68693e9a8f224715a8936dd49c01a
SeanCoLtd <loukjltd@gmail.com>
2021-04-22 09:24:20 +0800

    daily update

---
+++ RealHTML/阶段06 - Javascript基础语法/第01单元 - 基础语法/p06-u01-basic.js
@@ -0,0 +1,13 @@
+alert("Hello World!");
+alert('第二条信息');
+alert(3);
+
+document.write('这是一些向网页输出的内容。');
+document.write('<br>');
+document.write('这是另外一些向网页输出的内容。');
+document.write('<br>');
+document.write('这是一个左尖括号：&lt;');
+document.write('<br>');
+document.write('这是一个右尖括号：&gt;');
+
+console.log('这是一些向控制台输出的内容。');
