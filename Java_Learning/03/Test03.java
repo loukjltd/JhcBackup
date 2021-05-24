8b460b8326ecb42adb27bc0114689fbd2394b06e
SeanCoLtd <loukjltd@gmail.com>
2021-05-24 14:54:33 +0800

    Learning Java

---
+++ Java_Learning/03/Test03.java
@@ -0,0 +1,11 @@
+public class Test03 {
+    public static void main(String[] args) {
+        int age = 18;
+        int monthlySalary = 30000;
+        int annualSalary = monthlySalary * 15;
+        System.out.println(annualSalary);
+
+        double yearEndBonus = 1888.88;
+        System.out.println(yearEndBonus);
+    }
+}
