2e45f410cca74b01e0441ba20166fca0bca3a040
SeanCoLtd <loukjltd@gmail.com>
2021-05-25 11:48:58 +0800

    daily update

---
+++ Java_Learning/05The BasicDataTypeCHAR_EscapeCharacter_CharacterSet/TestBoolean.java
@@ -0,0 +1,8 @@
+public class TestBoolean {
+    public static void main(String[] args) {
+        boolean flag = true;
+        if (flag) {
+            System.out.println("Code");
+        }
+    }
+}
---
+++ Java_Learning/05The BasicDataTypeCHAR_EscapeCharacter_CharacterSet/TestChar.java
@@ -0,0 +1,17 @@
+public class TestChar {
+    public static void main(String[] args) {
+        char c1 = 'A';
+        char c2 = '汉';
+
+        System.out.print(c1);
+        System.out.print(c2);
+
+        char c4 = '族';
+
+        System.out.print(c4);
+
+        String str1 = "\n中华人民共和国";
+
+        System.out.println(str1);
+    }
+}
